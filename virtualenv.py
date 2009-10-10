cba7cdecdd1269ca9472e2ceb84538282aaae086
Brandon Craig Rhodes <brandon@rhodesmill.org>
2009-10-10 01:02:35 -0400

    Removed two additional checks for Python 2 version numbers.

--- virtualenv.py
+++ virtualenv.py
@@ -20,12 +20,8 @@ REQUIRED_MODULES = ['os', 'posix', 'posixpath', 'ntpath', 'genericpath',
                     'fnmatch', 'locale', 'encodings', 'codecs',
                     'stat', 'UserDict', 'readline', 'copy_reg', 'types',
                     're', 'sre', 'sre_parse', 'sre_constants', 'sre_compile',
-                    'lib-dynload', 'config', 'zlib']
-
-if sys.version_info[:2] == (2, 6):
-    REQUIRED_MODULES.extend(['warnings', 'linecache', '_abcoll', 'abc'])
-if sys.version_info[:2] <= (2, 3):
-    REQUIRED_MODULES.extend(['sets', '__future__'])
+                    'lib-dynload', 'config', 'zlib', 'warnings', 'linecache',
+                    '_abcoll', 'abc']
 
 class Logger(object):
 
