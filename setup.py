252565cb82ca4c649376615b892d0dbb65a61b6f
Brandon Craig Rhodes <brandon@rhodesmill.org>
2011-05-11 18:43:49 -0400

    Last rites: for a final "setup.py" register, created a description that should point everyone at the main "virtualenv" project.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     from distutils.core import setup
     print('Note: without Setuptools installed you will have to use "python3 -m virtualenv3 ENV"')
 
-version = '1.3.4.2'
+version = '0.0'
 
 f = open(os.path.join(os.path.dirname(__file__), 'docs', 'index.txt'))
 long_description = f.read().strip()
@@ -18,27 +18,6 @@ f.close()
 
 setup(name='virtualenv3',
       version=version,
-      description="Virtual Python 3 Environment builder",
+      description='Obsolete fork of virtualenv',
       long_description=long_description,
-      classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-      ],
-      keywords='setuptools deployment installation distutils',
-      author='Ian Bicking',
-      author_email='ianb@colorstudy.com',
-      url='http://bitbucket.org/brandon/virtualenv3/',
-      license='MIT',
-      py_modules=['virtualenv3'],
-      ## Hacks to get the package data installed:
-      packages=[''],
-      package_dir={'': '.'},
-      package_data={'': ['support-files/*-py%s.egg' % sys.version[:3]]},
-      zip_safe=False,
-      entry_points="""
-      [console_scripts]
-      virtualenv3 = virtualenv3:main
-      """,
       )
